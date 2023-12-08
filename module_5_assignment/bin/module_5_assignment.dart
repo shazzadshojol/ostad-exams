ec4ea7bb6e8d616739d0f2fadd55203fa827a74f
shazzadshojol <shazzadshojol@gmail.com>
2023-12-08 22:13:46 +0600

    Exams

---
+++ module_5_assignment/bin/module_5_assignment.dart
@@ -0,0 +1,5 @@
+import 'package:module_5_assignment/module_5_assignment.dart' as module_5_assignment;
+
+void main(List<String> arguments) {
+  print('Hello world: ${module_5_assignment.calculate()}!');
+}
---
+++ module_5_assignment/test/module_5_assignment_test.dart
@@ -0,0 +1,8 @@
+import 'package:module_5_assignment/module_5_assignment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
