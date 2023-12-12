0fd588c23ae1514c565c64f78933084d4c1ad236
shazzadshojol <shazzadshojol@gmail.com>
2023-12-12 09:08:21 +0600

    Main Commit

--- module_5_live_test/bin/main.dart
+++ module_5_live_test/bin/main.dart
@@ -1,3 +1,30 @@
 void main() {
-  print('hello world');
+  List fruits = [
+    {'Name': 'Apple', 'Color': 'Red', 'price': 2.5},
+    {'Name': 'Banana', 'Color': 'Yellow', 'price': 1.0},
+    {'Name': 'Grapes', 'Color': 'Purple', 'price': 3.0},
+  ];
+  print('Original Fruit Details before Discount:');
+  displayFruitDetails(fruits);
+
+  print('Fruit Details After Applying 10% Discount:');
+  priceDiscount(fruits, 10);
+
+  print("\nFruit Details After Applying 10% Discount:");
+  displayFruitDetails(fruits);
+}
+
+void displayFruitDetails(List fruitsList) {
+  for (var i = 0; i < fruitsList.length; i++) {
+    var fruit1 = fruitsList[i];
+    print(
+        "Name: ${fruit1['Name']},Color: ${fruit1['Color']},Price: ${fruit1['price']}");
+  }
+}
+
+void priceDiscount(List fruit2, double discount) {
+  for (var i = 0; i < fruit2.length; i++) {
+    fruit2[i]['price'] =
+        fruit2[i]['price'] - (fruit2[i]['price'] * discount / 100);
+  }
 }
